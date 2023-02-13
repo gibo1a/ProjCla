c69512803c24eba9e26be9d95b80531dcc0d3ab3
gibo1a <gabiboia10@gmail.com>
2023-02-13 13:57:02 +0000

    projetofinaliei

---
+++ IEI/Projeto/script.js
@@ -0,0 +1,9 @@
+function show(){
+    var h = document.getElementById("scrollbox1");
+    if(h.style.display === 'none'){
+        h.style.display = 'block';
+    } else {
+        h.style.display = 'block';
+    }
+
+}
