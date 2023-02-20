a68902a1fa15776f39823233e3c48b932dc6d9c4
gibo1a <gabiboia10@gmail.com>
2023-02-20 15:52:34 +0000

    aula labi

--- Labi/cifraComARC4.py
+++ Labi/cifraComARC4.py
@@ -1,27 +1,26 @@
 from Crypto.Hash import SHA256
 from Crypto.Cipher import ARC4
 import os
+import sys
 
-def encriptar(ficheiro):
-    with open(ficheiro, 'rb') as file:
-        buffer = file.read(256)
-        while len(buffer) > 0:
-            if len(buffer) < 5:
-                h = SHA256.new()
-                for i in file:
-                    h.update(i.encode('utf-8'))
-                print(h.hexdigest())
-            elif len(buffer) > 256:
-                buffer1 = buffer[:257]
-                cipher = ARC4.new('chave')
-                cryptogram = cipher.encrypt('buffer1')
-                os.write(1, cryptogram)
-                print()
-            else:
-                cipher = ARC4.new('chave')
-                cryptogram = cipher.encrypt('buffer')
-                os.write(1, cryptogram)
-                print()
-            buffer = file.read(256)
+if sys.argv < 3:
+    quit()
+elif not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]) or os.path.isdir(sys.argv[1]):
+    quit()
 
+if sys.argv[2] < 5:
+    h = SHA256.new()
+    h.update(sys.argv[2].encode('utf-8'))
+    key = h.digest()
+else:
+    key = sys.argv[2][:256]
+
+with open(sys.argv[1],'rb') as file:
+    cipher = ARC4.new(key)
+    for i in file:
+        criptograma = cipher.encrypt(i)
+        os.write(1,criptograma)
+        print()
+
+    
                 
--- Labi/cifracomAES.py
+++ Labi/cifracomAES.py
@@ -1,5 +1,22 @@
 from Crypto.Cipher import AES
+import os
+import sys
+from Crypto.Hash import SHA256
+if(sys.argv) < 3:
+    quit()
+elif not os.path.exists(sys.argv[1]) or os.path.isdir(sys.argv[1]) or not os.path.isfile(sys.argv[1]):
+    print(sys.argv[1] + 'cannot be encrypted or decrypted', file=sys.stderr)
+    quit()
 
-key = '1234567890abcdef'
+if(sys.argv[2]) < 16:
+    h = SHA256.new()
+    h.update(sys.argv[2].encode('utf-8'))
+    key = h.digest()
+else:
+    key = sys.argv[2][:256]
+
 cipher = AES.new(key)
-print(cipher.mode)
+
+with open(sys.argv[1], 'rb') as file:
+    x = ''.join(cipher.encrypt(i) for i in file)
+    os.write(1,x)
