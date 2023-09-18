45c6bc4aaec0e4746bace8b414f190d7e4006b61
gibo1a <gabiboia10@gmail.com>
2023-09-18 19:27:48 +0100

    exemplos das aulas + resoluçao dos exercicios

--- AED/java_toc.c
+++ AED/java_toc.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int printArray(char s,int a[]){
-    printf("%c : ",s);
-    for(int i = 0; i<sizeof(a)/sizeof(a[0]);i++){
+int printArray(char s[], int a[]){
+    printf("%s:",s);
+    for(int i = 0;i < 12;i++){
         printf("%d ",a[i]);
     }
     printf("\n");
@@ -11,20 +11,19 @@ int printArray(char s,int a[]){
 
 int cumSum(int a[],int b[]){
     int c = 0;
-    for(int i = 0;i<sizeof(a)/sizeof(a[0]);i++){
+    for(int i = 0;i<12;i++){
         c += a[i];
         b[i] = c;
 
     }
-
     return 0;
 }
 
 int main(void){
     int a[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    printArray("a",a[12]);
+    printArray("a",a);
     int b[12];
-    cumSum(a[12],b[12]);
-    printArray("b",b[12]);
+    cumSum(a,b);
+    printArray("b",b);
     return 0;
 }
---
+++ AED/test_file1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int sum(char s){
+    printf("%c",s);
+    return 0;
+}
+
+int main(void){
+    printf("%s","a");
+    return 0;
+}
