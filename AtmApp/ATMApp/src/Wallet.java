ec6431ba49a37903058375e06c3f4cdea5525390
gibo1a <gabiboia10@gmail.com>
2023-10-26 11:20:48 +0100

    guiao 5 aed

---
+++ AtmApp/ATMApp/src/Wallet.java
@@ -0,0 +1,23 @@
+public class Wallet {
+    private int funds;
+
+    public Wallet(){
+    }
+
+    public Wallet(int funds){
+        this.funds = funds;
+    }
+
+    public int getFunds(){
+        return funds;
+    }
+
+    public void setFunds(int funds){
+        this.funds = funds;
+    }
+
+
+
+
+    
+}
