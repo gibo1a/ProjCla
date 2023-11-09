53853acba4eafebde7d5388172bfb3f1fbf1411c
gibo1a <gabiboia10@gmail.com>
2023-11-09 12:49:06 +0000

    guiao8

---
+++ PersonSet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "PersonSet.h"
+
+typedef struct PersonSet{
+    Person **array;
+};
+
+PersonSet* PersonSetCreate(){
+
+}
+
+void PersoSetDestroy(PersonSet **pps){
+
+}
